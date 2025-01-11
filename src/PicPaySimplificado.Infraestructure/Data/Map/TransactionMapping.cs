499abb00ac01223b38e84f4e8d86567a85bf913c
Wellington <wellington.climaco9@gmail.com>
2025-01-11 18:15:44 -0300

    feat: mapping table

---
+++ src/PicPaySimplificado.Infraestructure/Data/Map/TransactionMapping.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PicPaySimplificado.Core.Entities;
+using PicPaySimplificado.Infraestructure.Extensions;
+
+namespace PicPaySimplificado.Infraestructure.Data.Map
+{
+    internal class TransactionMapping : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.MappingBase();
+        }
+    }
+}
---
+++ src/PicPaySimplificado.Infraestructure/Data/Map/WalletMapping.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PicPaySimplificado.Core.Entities;
+using PicPaySimplificado.Infraestructure.Extensions;
+
+namespace PicPaySimplificado.Infraestructure.Data.Map
+{
+    public class WalletMapping : IEntityTypeConfiguration<Wallet>
+    {
+        public void Configure(EntityTypeBuilder<Wallet> builder)
+        {
+            builder.MappingBase();         
+        }
+    }
+}

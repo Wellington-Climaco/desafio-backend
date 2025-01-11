0dafc3ddd912ca224eed883c556e3d3a25ffa624
Wellington <wellington.climaco9@gmail.com>
2025-01-11 18:18:57 -0300

    feat: add wallet controller

---
+++ src/PicPaySimplificado.Api/Controllers/WalletController.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using PicPaySimplificado.Application.Interface;
+using PicPaySimplificado.Application.Request;
+using PicPaySimplificado.Application.Validation.Wallet;
+
+namespace PicPaySimplificado.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class WalletController : ControllerBase
+    {
+        private readonly IWalletService _walletService;
+        private readonly IValidator<CreateWalletRequest> _createWalletValidation;
+        public WalletController(IWalletService walletService, IValidator<CreateWalletRequest> createWalletValidation)
+        {
+            _walletService = walletService;
+            _createWalletValidation = createWalletValidation;
+        }
+
+        [HttpPost("register")]  
+        public async Task<IActionResult> CreateWallet(CreateWalletRequest request)
+        {
+            var validation = await _createWalletValidation.ValidateAsync(request);
+
+            if (!validation.IsValid)
+                return BadRequest(validation.Errors.Select(x=>x.ErrorMessage));
+
+            var result = await _walletService.AddWallet(request);
+
+            if(!result.IsSuccess)
+                return Conflict(result.Errors);
+
+            return Ok("Wallet created with success");
+        }
+    }
+}
--- src/PicPaySimplificado.Api/Program.cs
+++ src/PicPaySimplificado.Api/Program.cs
@@ -1,5 +1,5 @@
-
-using PicPaySimplificado.Infraestructure;
+using PicPaySimplificado.Application;
+using PicPaySimplificado.Infraestructure.Extensions;
 
 namespace PicPaySimplificado.Api
 {
@@ -15,6 +15,7 @@ namespace PicPaySimplificado.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddApplication();
             builder.Services.AddInfra(builder.Configuration);
 
             var app = builder.Build();

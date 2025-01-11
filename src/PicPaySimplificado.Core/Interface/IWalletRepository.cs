747ddce35e866ab60fc6125c3ee88471ac67e0ec
Wellington <wellington.climaco9@gmail.com>
2025-01-11 18:11:41 -0300

    feat: add wallet repository

---
+++ src/PicPaySimplificado.Core/Interface/IWalletRepository.cs
@@ -0,0 +1,11 @@
+﻿using PicPaySimplificado.Core.Entities;
+
+namespace PicPaySimplificado.Core.Interface
+{
+    public interface IWalletRepository
+    {
+        Task<Wallet> CreateWallet(Wallet wallet);
+        Task<Wallet?> FindByEmail(string email);
+        Task<Wallet?> FindByDocument(string cpfcnpj);
+    }
+}
---
+++ src/PicPaySimplificado.Infraestructure/Repository/WalletRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PicPaySimplificado.Core.Entities;
+using PicPaySimplificado.Core.Interface;
+using PicPaySimplificado.Infraestructure.Data;
+
+namespace PicPaySimplificado.Infraestructure.Repository
+{
+    internal class WalletRepository : IWalletRepository
+    {
+        private readonly AppDbContext _dbcontext;
+        public WalletRepository(AppDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public async Task<Wallet> CreateWallet(Wallet wallet)
+        {
+            await _dbcontext.AddAsync(wallet);
+            await _dbcontext.SaveChangesAsync();
+            return wallet;
+        }
+
+        public async Task<Wallet?> FindByDocument(string cpfcnpj)
+        {
+            return await _dbcontext.Wallet.FirstOrDefaultAsync(x=>x.Cpf == cpfcnpj);
+        }
+
+        public async Task<Wallet?> FindByEmail(string email)
+        {
+            return await _dbcontext.Wallet.FirstOrDefaultAsync(x => x.Email == email);
+        }
+    }
+}

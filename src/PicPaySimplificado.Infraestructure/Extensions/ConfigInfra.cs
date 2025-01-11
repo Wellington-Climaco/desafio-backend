2d8b7f692fa2d36a2ff15a09516497f81883b13c
Wellington <wellington.climaco9@gmail.com>
2025-01-11 18:16:13 -0300

    feat: config dependency injection

---
+++ src/PicPaySimplificado.Infraestructure/Extensions/ConfigInfra.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PicPaySimplificado.Core.Interface;
+using PicPaySimplificado.Infraestructure.Data;
+using PicPaySimplificado.Infraestructure.Repository;
+
+namespace PicPaySimplificado.Infraestructure.Extensions
+{
+    public static class ConfigInfra
+    {
+        public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
+        {
+            DependencyInjection(services);
+            ConfigDbContext(configuration, services);
+            return services;
+        }
+
+        public static void ConfigDbContext(IConfiguration configuration, IServiceCollection services)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(x => x.UseNpgsql(connectionString));
+        }
+
+        public static void DependencyInjection(IServiceCollection services)
+        {
+            services.AddScoped<IWalletRepository,WalletRepository>();
+        }
+    }
+}

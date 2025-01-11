9f1d797c6760c85ab16f878ae80cd725683e225a
Wellington <wellington.climaco9@gmail.com>
2025-01-11 18:18:40 -0300

    feat: application

---
+++ src/PicPaySimplificado.Application/ConfigApplication.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using PicPaySimplificado.Application.Interface;
+using PicPaySimplificado.Application.Request;
+using PicPaySimplificado.Application.Services;
+using PicPaySimplificado.Application.Validation.Wallet;
+
+namespace PicPaySimplificado.Application
+{
+    public static class ConfigApplication
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IWalletService, WalletService>();
+
+            ConfigValidators(services);
+            return services;
+        }
+
+        public static void ConfigValidators(IServiceCollection services)
+        {
+            services.AddScoped<IValidator<CreateWalletRequest>,CreateWalletValidation>();
+        }
+    }
+}
---
+++ src/PicPaySimplificado.Application/Request/CreateWalletRequest.cs
@@ -0,0 +1,11 @@
+﻿using PicPaySimplificado.Core.Entities;
+
+namespace PicPaySimplificado.Application.Request
+{
+    public record CreateWalletRequest(string Nome, string Cpfcnpj, string Email, string Password, string TipoCarteira)
+    {
+        public Wallet toEntity() => new Wallet(Nome, Cpfcnpj, Email, Password, TipoCarteira);
+    }
+
+    
+}

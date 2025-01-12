5732a4a4e6474826abb7f9623675de2106112487
Wellington <wellington.climaco9@gmail.com>
2025-01-12 16:08:41 -0300

    refactor: rename entity

--- src/PicPaySimplificado.Core/Entities/Transference.cs
+++ src/PicPaySimplificado.Core/Entities/Transference.cs
@@ -3,22 +3,22 @@ using PicPaySimplificado.Core.Enum;
 
 namespace PicPaySimplificado.Core.Entities
 {
-    public class Transaction : EntityBase
+    public class Transference : EntityBase
     {
 
 
         //orm
-        protected Transaction() { }
+        protected Transference() { }
 
-        public Transaction(StatusTransaction status, decimal value, Wallet payeer, Wallet reciver)
+        public Transference(StatusTransaction status, decimal value, Wallet payeer, Wallet reciver)
         {
-            Status = status;
+            Status = status.ToString();
             Value = value;
             Payeer = payeer;
             Reciver = reciver;
         }
 
-        public StatusTransaction Status { get; set; }
+        public string Status { get; set; }
         public decimal Value { get; set; }
 
         public Wallet Payeer { get; set; }
@@ -26,6 +26,12 @@ namespace PicPaySimplificado.Core.Entities
 
         public Wallet Reciver { get; set; }
         public Guid ReciverId { get; set; }
+
+
+        public void ChangeStatus(StatusTransaction status)
+        {
+            Status = status.ToString();
+        }
 
     }
 }
